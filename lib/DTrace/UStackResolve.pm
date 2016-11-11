6807a240f82806da3fd7f61ce3bbec62367317e8
Gordon Marler <gmarler@gmarler.com>
2016-11-11 11:10:09 -0500

    Eliminate autoflush of the reading of unresolved DTrace

--- lib/DTrace/UStackResolve.pm
+++ lib/DTrace/UStackResolve.pm
@@ -1395,8 +1395,6 @@ sub start_stack_resolve {
   #       debugging in the case where a stack resolution fails.
   my $filestream = IO::Async::FileStream->new(
     read_handle => $dtrace_unresolved_fh,
-    # TODO: Eliminate autoflush
-    autoflush   => $self->autoflush_dtrace_output,
     on_initial => sub {
       my ( $self ) = @_;
       say "FileStream on_initial self type: " . ref( $self );
